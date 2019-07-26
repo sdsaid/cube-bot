12b6cf86c1e3329a3b32398962aef6de36bb467e
Said Said <Said8022@gmail.com>
2019-07-26 05:04:15 -0700

    added stepper

--- motor_control.py
+++ motor_control.py
@@ -23,3 +23,30 @@ for i in range(512):
       GPIO.output(control_pins[pin], halfstep_seq[halfstep][pin])
     time.sleep(0.001)
 GPIO.cleanup()
+
+
+
+#initialize GPIO pins
+GPIO.setmode(GPIO.BOARD)
+control_pins = [7,11,13,15]
+  for pin in control_pins:
+    GPIO.setup(pin, GPIO.OUT)
+    GPIO.output(pin, 0)
+  halfstep_seq = [
+    [1,0,0,0],
+    [1,1,0,0],
+    [0,1,0,0],
+    [0,1,1,0],
+    [0,0,1,0],
+    [0,0,1,1],
+    [0,0,0,1],
+    [1,0,0,1]
+                                                              
+                                                              
+                                                              
+                                                              
+                                                              ]
+
+#motor turn function
+def clockwise(self, deg):
+  self.deg = 
--- solver.py
+++ solver.py
@@ -1,1 +1,15 @@
 import kociemba 
+
+
+
+#u = kociemba.solve('DRLUUBFBRBLURRLRUBLRDDFDLFUFUFFDBRDUBRUFLLFDDBFLUBLRBD')
+
+#print(u)
+
+z = kociemba.solve('ULBUUUULDBBRDRDLDRRULRFBDLFLFURDFBDFBFFULFLBFDBRLBRDRU')
+print(z)
+
+for i in z:
+    if i == 'D':
+        clockwise()
+    
