074e8dcb1ff14b44ee0cd9598c9a59a109250af7
Said Said <Said8022@gmail.com>
2019-07-28 20:07:15 -0700

    added motor turns

--- motor_control.py
+++ motor_control.py
@@ -1,34 +1,13 @@
 import RPi.GPIO as GPIO
 import time 
 
-GPIO.setmode(GPIO.BOARD)
+#"Down Motor."
 
-control_pins = [7,11,13,15]
-for pin in control_pins:
-  GPIO.setup(pin, GPIO.OUT)
-  GPIO.output(pin, 0)
-halfstep_seq = [
-  [1,0,0,0],
-  [1,1,0,0],
-  [0,1,0,0],
-  [0,1,1,0],
-  [0,0,1,0],
-  [0,0,1,1],
-  [0,0,0,1],
-  [1,0,0,1]
-]
-for i in range(512):
-  for halfstep in range(8):
-    for pin in range(4):
-      GPIO.output(control_pins[pin], halfstep_seq[halfstep][pin])
-    time.sleep(0.001)
-GPIO.cleanup()
-
-
-
-#initialize GPIO pins
 GPIO.setmode(GPIO.BOARD)
 control_pins = [7,11,13,15]
+
+#clockwise rotation function
+def clockwise():
   for pin in control_pins:
     GPIO.setup(pin, GPIO.OUT)
     GPIO.output(pin, 0)
@@ -41,12 +20,34 @@ control_pins = [7,11,13,15]
     [0,0,1,1],
     [0,0,0,1],
     [1,0,0,1]
-                                                              
-                                                              
-                                                              
-                                                              
-                                                              ]
+  ]
+  for i in range(128):
+    for halfstep in range(8):
+      for pin in range(4):
+        GPIO.output(control_pins[pin], halfstep_seq[halfstep][pin])
+      time.sleep(0.001)
 
-#motor turn function
-def clockwise(self, deg):
-  self.deg = 
+
+
+#counterclockwise motor turn (90 degrees)
+def counter():
+  for pin in control_pins:
+    GPIO.setup(pin, GPIO.OUT)
+    GPIO.output(pin, 0)
+  halfstep_seq = [
+    [1,0,0,1],
+    [0,0,0,1],
+    [0,0,1,1],
+    [0,0,1,0],
+    [0,1,1,0],
+    [0,1,0,0],
+    [0,1,0,0],
+    [1,0,0,0]
+    ]
+  for i in range(128):
+    for halfstep in range(8):
+      for pin in range(4):
+        GPIO.output(control_pins[pin], (halfstep_seq)[halfstep][pin])
+      time.sleep(0.001)
+
+GPIO.cleanup()
--- solver.py
+++ solver.py
@@ -1,4 +1,58 @@
-import kociemba 
+import kociemba
+
+import RPi.GPIO as GPIO
+import time 
+
+#"Down Motor."
+
+GPIO.setmode(GPIO.BOARD)
+control_pins = [7,11,13,15]
+
+#clockwise rotation function
+def clockwise():
+  for pin in control_pins:
+    GPIO.setup(pin, GPIO.OUT)
+    GPIO.output(pin, 0)
+  halfstep_seq = [
+    [1,0,0,0],
+    [1,1,0,0],
+    [0,1,0,0],
+    [0,1,1,0],
+    [0,0,1,0],
+    [0,0,1,1],
+    [0,0,0,1],
+    [1,0,0,1]
+  ]
+  for i in range(128):
+    for halfstep in range(8):
+      for pin in range(4):
+        GPIO.output(control_pins[pin], halfstep_seq[halfstep][pin])
+      time.sleep(0.001)
+
+
+
+#counterclockwise motor turn (90 degrees)
+def counter():
+  for pin in control_pins:
+    GPIO.setup(pin, GPIO.OUT)
+    GPIO.output(pin, 0)
+  halfstep_seq = [
+    [1,0,0,1],
+    [0,0,0,1],
+    [0,0,1,1],
+    [0,0,1,0],
+    [0,1,1,0],
+    [0,1,0,0],
+    [0,1,0,0],
+    [1,0,0,0]
+    ]
+  for i in range(128):
+    for halfstep in range(8):
+      for pin in range(4):
+        GPIO.output(control_pins[pin], (halfstep_seq)[halfstep][pin])
+      time.sleep(0.001)
+
+GPIO.cleanup()
 
 
 
@@ -12,4 +66,4 @@ print(z)
 for i in z:
     if i == 'D':
         clockwise()
-    
+
